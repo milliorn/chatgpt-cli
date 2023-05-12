5d9f7aee45afce704b6e6fb08272d2039ca8ebe2
milliorn <scottmilliorn@gmai.com>
2023-05-12 14:21:58 -0700

    userInterface

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import readline from "readline";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 dotenv.config();
-console.log(process.env); // remove this after you've confirmed it is working
+// console.log(process.env); // remove this after you've confirmed it is working
 
 // OpenAI configuration
 
@@ -15,18 +15,29 @@ const configuration = new Configuration({
 // create new instance of OpenAI API
 const openai = new OpenAIApi(configuration);
 
-// calls the createChatCompletion, triggers an endpoint (https://api.openai.com/v1/chat/completions).
-// function accepts an object of arguments, the model of chatGPT in use, an array of messages between user and AI.
-// each message is an object containing the role, sent the message.
-// value can be assistant, if from the AI or user,
-// when the message originates user, and the content.
-// the code prints the response from the AI.
-openai
-  .createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello" }],
-  })
-  .then((res) => {
-    console.log(res.data.choices[0].message.content);
-  })
-  .catch((e) => console.error(e));
+// creates a UI in the terminal that allows users to type in their questions.
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// prompt the user to enter a message
+userInterface.prompt();
+
+// user gives prompt and hits Enter it triggers a callback
+// which passes as input which is now used as content.
+// when response is displayed the user is prompted for another message.
+userInterface.on("line", async (input) => {
+  await openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: input }],
+    })
+    .then((res) => {
+      console.log(res.data.choices[0].message.content);
+      userInterface.prompt();
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+});
