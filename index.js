4e6869637c3fe5d748bee33f0867fe59fafb41c9
milliorn <scottmilliorn@gmai.com>
2023-05-12 13:56:46 -0700

    dotenv

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { Configuration, OpenAIApi } from "openai";
 import readline from "readline";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+dotenv.config();
+console.log(process.env); // remove this after you've confirmed it is working
 
 // OpenAI configuration
 
 const configuration = new Configuration({
-  organization: "",
-  apiKey: "",
+  organization: process.env.ORGANIZATION,
+  apiKey: process.env.API_KEY,
 });
