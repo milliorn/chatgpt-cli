ae6b5c2bf98215089550e30ca088a1ba2ccce530
milliorn <scottmilliorn@gmai.com>
2023-05-12 13:51:30 -0700

    configuration

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import { Configuration, OpenAIApi } from "openai";
 import readline from "readline";
+
+// OpenAI configuration
+
+const configuration = new Configuration({
+  organization: "",
+  apiKey: "",
+});
